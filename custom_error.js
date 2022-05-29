85caf5baa28d62ee924b2cf2580bfdba14d205a1
Dat Dao <dat.dao@c2c-techhub.io>
2022-05-29 14:07:59 +0700

    custom error with http-errors

--- custom_error.js
+++ custom_error.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const createError = require('http-errors');
+
 const express = require('express');
 
 const app = express();
@@ -24,24 +26,47 @@ app.use(morgan());
 app.use(express.json());
 
 // apply body-parser to express for Content-Type: application/x-www-form-urlencoded
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+// at first all routes
+app.use((req, res, next) => {
+  console.log(req.headers['x-my-token']);
+  if (!req.headers['x-my-token'])
+    return next(createError(401, 'Please login to view this page.'));
+  next();
+});
 
 // make new a route
 // query string
 app.all('/v1/custom-error', (req, res, next) => {
+  console.log(req.headers['x-my-token']);
   console.log(req.query);
   res.json({
     data: req.query,
   });
 });
 
-// manual handle custom error
-app.use((req, res, next) => {
-  const error = new Error('My Not Found!');
-  error.status = 404;
-  next(error);
+// make a error in controller
+app.all('/v1/make-error-500', (req, res, next) => {
+  console.log(`my variable ${a}`);
+  res.json({
+    data: req.query,
+  });
 });
 
+// manual handle custom error
+// app.use((req, res, next) => {
+//   const error = new Error('My Not Found!');
+//   error.status = 404;
+//   next(error);
+// });
+
+// handle custom error by package
+app.use((req, res, next) => {
+  next(createError(404, 'my not found by http-errors'));
+});
+
+// at the end all routes
 // get error from previous middleware
 app.use((error, req, res, next) => {
   res.json({
